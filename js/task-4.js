85d290c3274ced2ba0588a68c9035fe2eb5d5d02
Alena <alporokh@gmail.com>
2026-03-15 19:55:07 +0200

    update

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,25 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+
+const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+const emailValue = email.value.trim();
+const passwordValue = password.value.trim();
+
+if (emailValue === "" || passwordValue === "") {
+    return alert("All form fields must be filled in");
+  }
+const formData = {
+    email: emailValue,
+    password: passwordValue,
+  };
+
+console.log(formData);
+event.currentTarget.reset();
+}
